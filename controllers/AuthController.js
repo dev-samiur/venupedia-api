69c4ab75f88c7978bd96d1b64f643c7eb0467531
Samiur Rahman <samiurrahman.dev@gmail.com>
2021-12-15 14:24:39 +0600

    chore(auth): update response

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,44 +1,44 @@
-const User= require('../models/User');
+const User = require('../models/User');
 require('dotenv').config();
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-exports.login= async (req,res) => {
-    try {    
-  	    const email = req.body.email;
-        const password = req.body.password;
+exports.login = async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
 
-        const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-        if (!user) {
-            return res.json({ error: "Invalid login details" });
-        }
+    if (!user) {
+      return res.json({ error: 'Invalid login details' });
+    }
 
-        const isPasswordMatch = await bcrypt.compare(password, user.password);
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
 
-        if (!isPasswordMatch) {
-            return res.json({ error: "Invalid login details" });
-        }
+    if (!isPasswordMatch) {
+      return res.json({ error: 'Invalid login details' });
+    }
 
-        const token = jwt.sign({ id: user._id, email: user.email }, 'secret');
-        res.json({success: token});
-    }catch (err) {
-        res.json({ error: err });
-    }    
-}
+    const token = jwt.sign({ id: user._id, email: user.email }, 'secret');
+    res.json({ success: { token, user: { id: user._id, email: user.email } } });
+  } catch (err) {
+    res.json({ error: err });
+  }
+};
 
-exports.register= async (req, res) => {
-	const hashedPassword= await bcrypt.hash(req.body.password, 10)
-	try{
-			await User.create({
-					username: req.body.username,
-					email: req.body.email,
-					password: hashedPassword,
-					role: req.body.role,
-					balance: req.body.balance,
-			})
-			res.json({success: 'User registration successful'})
-	} catch(err){
-			res.json({error: err})
-	}
-}
+exports.register = async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  try {
+    await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+      role: req.body.role,
+      balance: req.body.balance,
+    });
+    res.json({ success: 'User registration successful' });
+  } catch (err) {
+    res.json({ error: err });
+  }
+};
