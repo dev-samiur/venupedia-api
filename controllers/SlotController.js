9ea68b928b96ed25eede31de2a9ab903d547a26f
Samiur Rahman <samiurrahman.dev@gmail.com>
2021-12-22 14:02:56 +0600

    feat(slots): add endpoints for slots

--- controllers/SlotController.js
+++ controllers/SlotController.js
@@ -1,9 +1,17 @@
 const Slot = require('../models/Slot');
+const Booking = require('../models/Booking');
 
 exports.getById = async (req, res) => {
   try {
-    let slots = await Slot.findById(req.params.id);
-    res.json({ success: slots });
+    const slots = await Slot.find({ venue_id: req.params.id });
+    const bookings = await Booking.find();
+    const bookedDates = bookings.map((booking) => booking.date);
+    const available = [];
+    slots.forEach((slot) => {
+      const exist = bookedDates.includes(slot.date);
+      if (!exist) available.push(slot);
+    });
+    res.json({ success: available });
   } catch (err) {
     res.json({ error: err.message });
   }
