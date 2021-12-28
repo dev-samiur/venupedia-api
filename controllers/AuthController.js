64d4180e95cfcff9d9dc60789ff802fdd8f72b25
Samiur Rahman <samiurrahman.dev@gmail.com>
2021-12-29 02:51:54 +0600

    lint: fix linter

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -20,8 +20,18 @@ exports.login = async (req, res) => {
       return res.json({ error: 'Invalid login details' });
     }
 
-    const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET);
-    res.json({ success: { token, userId: user._id, username: user.username, email: user.email } });
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET
+    );
+    res.json({
+      success: {
+        token,
+        userId: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
   } catch (err) {
     res.json({ error: err });
   }
--- models/User.js
+++ models/User.js
@@ -1,23 +1,22 @@
-const mongoose= require('mongoose');
+const mongoose = require('mongoose');
 
-const userSchema= new mongoose.Schema({
-		username: {
-			type: string,
-			required: true
-		},
-    email:{
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-		type: {
-			type: Number,
-			required: true
-	},
+const userSchema = new mongoose.Schema({
+  username: {
+    type: string,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: Number,
+    required: true,
+  },
 });
 
-
-module.exports= mongoose.model('User', userSchema, 'users');
+module.exports = mongoose.model('User', userSchema, 'users');
